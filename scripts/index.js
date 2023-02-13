26c554144a4dba6925cf8e35bc436a8b682d0980
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-02-13 13:32:10 +0300

    sprint4-draft2

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,23 @@ const editProfileButton=document.querySelector('.profile__edit-button');
 editProfileButton.addEventListener('click', openPopUp);
 
 function openPopUp() {
-   const inputProfileName=document.querySelector('.pop-up__text_name');
-   const inputProfileProfession=document.querySelector('.pop-up__text_profession');
-   const savedProfileName=document.querySelector('.profile__name');
-   const savedProfileProfession=document.querySelector('.profile__profession');
+  const inputProfileName=document.querySelector('.pop-up__text_name');
+  const inputProfileProfession=document.querySelector('.pop-up__text_profession');
+  const savedProfileName=document.querySelector('.profile__name');
+  const savedProfileProfession=document.querySelector('.profile__profession');
 
-   inputProfileName.value=savedProfileName.textContent;
-   inputProfileProfession.value=savedProfileProfession.textContent;
+  inputProfileName.value=savedProfileName.textContent;
+  inputProfileProfession.value=savedProfileProfession.textContent;
 
-   const popUpElement= document.querySelector('.pop-up');
-   popUpElement.classList.add('pop-up_opened');
+  const popUpElement= document.querySelector('.pop-up');
+  popUpElement.classList.add('pop-up_opened');
 }
 
 const saveProfileButton=document.querySelector('.pop-up__save-button');
 saveProfileButton.addEventListener('click', saveInput);
 
-function saveInput(evt) {
-  evt.preventDefault(); 
+function saveInput(event) {
+  event.preventDefault(); 
 
   const inputProfileName=document.querySelector('.pop-up__text_name');
   const inputProfileProfession=document.querySelector('.pop-up__text_profession');
@@ -40,6 +40,17 @@ function closePopUp() {
    popUpElement.classList.remove('pop-up_opened');
 }
 
+/*const likeChecked=document.querySelectorAll('.elements__like');
+for (let i=0; i<likeChecked.length; i++) {
+  likeChecked[i].addEventListener('click', checkedLike);
+}
 
-
-
+function checkedLike(event) {
+   const likeChecked=event.target;
+   console.log(likeChecked.classList);
+   if (likeChecked.classList.contains('elements__like_checked')) {
+      likeChecked.classList.remove('elements__like_checked');
+   } else {
+      likeChecked.classList.add('elements__like_checked');
+   }   
+}*/
