56db966c3232db788666fbe26e04047090a669c6
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-02-15 12:23:16 +0300

    sprint4-draft4

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,29 @@
 const editProfileButton=document.querySelector('.profile__edit-button');
-editProfileButton.addEventListener('click', openPopUp);
+const popUpElement= document.querySelector('.pop-up');
+const inputProfileName=document.querySelector('.pop-up__text_content_name');
+const inputProfileProfession=document.querySelector('.pop-up__text_content_profession');
+const savedProfileName=document.querySelector('.profile__name');
+const savedProfileProfession=document.querySelector('.profile__profession');
+const сloseButton=document.querySelector('.pop-up__close-button');
+const formElement=document.querySelector('.pop-up__form');
 
 function openPopUp() {
-  const inputProfileName=document.querySelector('.pop-up__text_name');
-  const inputProfileProfession=document.querySelector('.pop-up__text_profession');
-  const savedProfileName=document.querySelector('.profile__name');
-  const savedProfileProfession=document.querySelector('.profile__profession');
-
   inputProfileName.value=savedProfileName.textContent;
   inputProfileProfession.value=savedProfileProfession.textContent;
-
-  const popUpElement= document.querySelector('.pop-up');
   popUpElement.classList.add('pop-up_opened');
 }
 
-const saveProfileButton=document.querySelector('.pop-up__save-button');
-saveProfileButton.addEventListener('click', saveInput);
-
-function saveInput(event) {
-  event.preventDefault(); 
-
-  const inputProfileName=document.querySelector('.pop-up__text_name');
-  const inputProfileProfession=document.querySelector('.pop-up__text_profession');
-  const savedProfileName=document.querySelector('.profile__name');
-  const savedProfileProfession=document.querySelector('.profile__profession');
-
-  savedProfileName.textContent=inputProfileName.value;
-  savedProfileProfession.textContent=inputProfileProfession.value;
-
-  const popUpElement= document.querySelector('.pop-up');
+function closePopUp() {
   popUpElement.classList.remove('pop-up_opened');
 }
 
-const CloseButton=document.querySelector('.pop-up__close-button');
-CloseButton.addEventListener('click', closePopUp);
-
-function closePopUp() {
-   const popUpElement= document.querySelector('.pop-up');
-   popUpElement.classList.remove('pop-up_opened');
+function saveInput(event) {
+  event.preventDefault(); 
+  savedProfileName.textContent=inputProfileName.value;
+  savedProfileProfession.textContent=inputProfileProfession.value;
+  closePopUp();
 }
 
-/*const likeChecked=document.querySelectorAll('.elements__like');
-for (let i=0; i<likeChecked.length; i++) {
-  likeChecked[i].addEventListener('click', checkedLike);
-}
-
-function checkedLike(event) {
-   const likeChecked=event.target;
-   console.log(likeChecked.classList);
-   if (likeChecked.classList.contains('elements__like_checked')) {
-      likeChecked.classList.remove('elements__like_checked');
-   } else {
-      likeChecked.classList.add('elements__like_checked');
-   }   
-}*/
+editProfileButton.addEventListener('click', openPopUp);
+formElement.addEventListener('submit', saveInput);
+сloseButton.addEventListener('click', closePopUp);
